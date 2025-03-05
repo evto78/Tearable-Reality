7b0f12727c73643b3bf416bb5abfa8d1ef723e21
Daubenyyy <150286450+Daubenyyy@users.noreply.github.com>
2025-03-05 15:11:22 -0500

    made function scene director

--- DW2025PROTO/Assets/Scripts/SceneDirector.cs
+++ DW2025PROTO/Assets/Scripts/SceneDirector.cs
@@ -8,6 +8,7 @@ public class SceneDirector : MonoBehaviour
     string[] terrors = {"test", "test2"};
 
     public int terrorSelectedNum;
+    public int selected;
 
     string terrorSelected;
 
@@ -17,9 +18,31 @@ public class SceneDirector : MonoBehaviour
         terrorSelected = terrors[terrorSelectedNum];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void selector1()
     {
-        
+        selected = 0;
+    }
+
+    public void selector2()
+    {
+        selected = 1;
+    }
+
+    public void selector3()
+    {
+        selected = 2;
+    }
+
+    public void check()
+    {
+        if(selected == terrorSelectedNum)
+        {
+            Debug.Log("CORRECT");
+        }
+
+        if(selected != terrorSelectedNum)
+        {
+            Debug.Log("INCORRECT");
+        }
     }
 }
